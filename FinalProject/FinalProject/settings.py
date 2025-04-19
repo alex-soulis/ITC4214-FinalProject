0f44ea83a12d810444d549d724f70ab1e3ce7e28
Alexandros Soulis <a.soulis@acg.edu>
2025-04-19 22:18:38 +0300

    Commit of the debug
    Change the DEBUG to false

--- FinalProject/FinalProject/settings.py
+++ FinalProject/FinalProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v5$88oh6fn$(0^tr2tb7t8j37ko6tr^@m%(j5vzqnu$i(f=2o2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
